0ad05ba8cb2bebe99a4a7d645330aa6880b1d1b4
Saeed Ahmad Ibrahim Saif Kalban <H00293766@hct.ac.ae>
2018-11-26 22:26:22 +0400

    daa

--- Phezo_BookStore_Project/Phezo_BookStore_Project/Registration.aspx.cs
+++ Phezo_BookStore_Project/Phezo_BookStore_Project/Registration.aspx.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-///using System.Linq;
+//using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,6 +9,6 @@ public partial class Registration : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        int aa = 54;
+        //int aa = 54;
     }
 }
